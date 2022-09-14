d1dfc7b2c1dd10af0211ad899126fb2e5213d6ef
selvarain <muthamilselvan181595@gmail.com>
2022-09-14 12:46:51 +0530

    go

---
+++ first-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import ImageUrl from './ReactTask/imageurl/ImageUrl';
+// import './App.css';
+// import SetBg from './setBg';
+// import StarRating from './starRating/customs';
+// import './starRating/customs.css'
+import MainButton from './ReactTask/MainButton';
+// import MyButton from './ReactTask/MyButton';
+
+
+function App() {
+  // const myStyle={backgroundColor:"teal"}
+   return (
+    <div>
+      {/* <SetBg/>
+      <StarRating/> */}
+      <MainButton/>
+      {/* <ImageUrl/> */}
+    </div>
+   )
+}
+
+export default App;
